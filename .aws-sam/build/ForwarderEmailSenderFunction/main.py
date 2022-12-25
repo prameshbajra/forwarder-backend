530c86da16d8bfaff112f481625241a6ce672eec
prameshbajra <pe.messh@gmail.com>
2022-12-25 19:59:03 +0545

    Email with device details done.

--- .aws-sam/build/ForwarderEmailSenderFunction/main.py
+++ .aws-sam/build/ForwarderEmailSenderFunction/main.py
@@ -5,7 +5,7 @@ from utilities import send_email
 def lambda_handler(event, context):
     event_body = json.loads(event['body'])
     sender_address = event_body['sender_address']
-    device_details = event_body['device_details']
+    device_details = json.loads(event_body['device_details'])
     message = event_body['message']
     print(event_body)
     is_email_sent = send_email(sender_address=sender_address,
